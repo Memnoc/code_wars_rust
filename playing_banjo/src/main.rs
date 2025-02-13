87fd0eaca2a62bb4a9aa8d2b408cc0bca940df90
Memnoc <smartdroidesign@gmail.com>
2025-02-13 09:27:51 +0000

    faster and more idiomatic, but does not handle unicode - not prod advised

--- playing_banjo/src/main.rs
+++ playing_banjo/src/main.rs
@@ -1,17 +1,26 @@
 // NOTE: create a function which answers the question
 // "Are you playing banjo?"
 // If your name starts with "R" or lower case "r" -> you are playing the banjo!
+// fn are_you_playing_banjo(name: &str) -> String {
+//     format!(
+//         "{} {} banjo",
+//         name,
+//         if name.starts_with(|c: char| c.eq_ignore_ascii_case(&'r')) {
+//             "plays"
+//         } else {
+//             "does not play"
+//         }
+//     )
+// }
+
+// A better, more idiomatic version
 fn are_you_playing_banjo(name: &str) -> String {
-    format!(
-        "{} {} banjo",
-        name,
-        if name.starts_with(|c: char| c.eq_ignore_ascii_case(&'r')) {
-            "plays"
-        } else {
-            "does not play"
-        }
-    )
+    match &name[0..1] {
+        "R" | "r" => format!("{} plays banjo", name),
+        _ => format!("{} does not play banjo", name),
+    }
 }
+
 fn main() {
     println!("=== Playing Banjo ===");
     let name_one = "Rikke";
