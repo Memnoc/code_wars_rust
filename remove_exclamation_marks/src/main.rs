66c772f8a998a4eb6ef5164bff77538c3dba76e2
Memnoc <smartdroidesign@gmail.com>
2025-02-17 06:46:41 +0000

    more elegant solution

--- remove_exclamation_marks/src/main.rs
+++ remove_exclamation_marks/src/main.rs
@@ -4,6 +4,13 @@ fn remove_exclamation_marks(input: &str) -> String {
     let input_string: String = input.to_string();
     input_string.chars().filter(|&c| c != '!').collect()
 }
+
+// NOTE: no significant drawbacks here, just a shorter
+// and more elegant version
+fn simpler_remove_exclamation_marks(input: &str) -> String {
+    input.replace("!", "")
+}
+
 fn main() {
     println!("=== Remove Exclamation Marks ===");
     let result = remove_exclamation_marks("Hello World!");
