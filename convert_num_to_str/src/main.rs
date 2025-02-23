b80fea6c5f50f8666b4a3e93640cf5c73ff12bbb
Memnoc <smartdroidesign@gmail.com>
2025-02-23 08:40:03 +0000

    convert num to string - init

---
+++ convert_num_to_str/src/main.rs
@@ -0,0 +1,7 @@
+// NOTE: https://www.codewars.com/kata/5265326f5fda8eb1160004c8/rust
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+fn main() {
+    println!("=== Convert a Number to a String ===");
+}
