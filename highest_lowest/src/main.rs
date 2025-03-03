729a67ab9a89294c7ba1f1754f794bfa8b213e56
Memnoc <smartdroidesign@gmail.com>
2025-03-03 06:05:04 +0000

    highest lowest soultion and test

--- highest_lowest/src/main.rs
+++ highest_lowest/src/main.rs
@@ -1,8 +1,22 @@
 fn high_and_low(numbers: &str) -> String {
-    todo!()
+    let nums: Vec<i32> = numbers
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    let max = nums.iter().max().unwrap();
+    let min = nums.iter().min().unwrap();
+
+    format!("{} {}", max, min)
 }
+
+// NOTE: given a string of space separated numbers,
+// return the highest and lowest number.
 fn main() {
     println!("=== Highest and Lowest ===");
+    high_and_low("1 2 3 4 5");
+    high_and_low("1 2 -3 4 5");
+    high_and_low("1 9 3 4 -5");
 }
 
 #[cfg(test)]
@@ -10,7 +24,10 @@ mod test {
     use super::*;
 
     #[test]
-    fn name() {
-        todo!();
+    fn test_high_and_low() {
+        assert_eq!(high_and_low("1 2 3 4 5"), "5 1");
+        assert_eq!(high_and_low("1 2 -3 4 5"), "5 -3");
+        assert_eq!(high_and_low("1 9 3 4 -5"), "9 -5");
+        assert_eq!(high_and_low("8"), "8 8");
     }
 }
