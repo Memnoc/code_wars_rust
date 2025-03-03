60511956bdde9b78fda4864071723f0ae851f4a1
Memnoc <smartdroidesign@gmail.com>
2025-03-03 05:20:38 +0000

    highest lowest init

---
+++ highest_lowest/src/main.rs
@@ -0,0 +1,16 @@
+fn high_and_low(numbers: &str) -> String {
+    todo!()
+}
+fn main() {
+    println!("=== Highest and Lowest ===");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn name() {
+        todo!();
+    }
+}
