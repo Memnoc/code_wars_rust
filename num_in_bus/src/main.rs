9a34bed49db53eff727b6ef095ca840a3c4c05ac
Memnoc <smartdroidesign@gmail.com>
2025-03-04 20:33:31 +0000

    number of people in the bus solution

--- num_in_bus/src/main.rs
+++ num_in_bus/src/main.rs
@@ -1,9 +1,34 @@
 fn number(bus_stops: &[(i32, i32)]) -> i32 {
-    todo!()
+    let people_on: i32 = bus_stops.iter().map(|&(on, _)| on).sum();
+    let people_off: i32 = bus_stops.iter().map(|&(_, off)| off).sum();
+
+    people_on - people_off
 }
 
+// INFO: There is a bus moving in the city which takes and drops some people at each bus stop.
+// You are provided with a list (or array) of integer pairs.
+// Elements of each pair represent the number of people that get on the bus (the first item)
+// and the number of people that get off the bus (the second item) at a bus stop.
+// ***********************************************************
+// HEADER: Your task is to return the number of people who are still on the bus
+// after the last bus stop (after the last array).
+// Even though it is the last bus stop, the bus might not be empty
+// and some people might still be inside the bus,
+// they are probably sleeping there :D"
+//
+// NOTE: easiset way would be to sum the first element of the array
+// then the second, then subtract them
 fn main() {
     println!("=== Number of People in the Bus ===");
+    println!("{}", number(&[(10, 0), (3, 5), (5, 8)]));
+    println!(
+        "{}",
+        number(&[(3, 0), (9, 1), (4, 10), (12, 2), (6, 1), (7, 10)])
+    );
+    println!(
+        "{}",
+        number(&[(3, 0), (9, 1), (4, 8), (12, 2), (6, 1), (7, 8)])
+    );
 }
 
 #[cfg(test)]
