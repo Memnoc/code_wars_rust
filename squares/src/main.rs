1d8b68555b31bd7a7c852d2f65662c76f1a2ac52
Memnoc <smartdroidesign@gmail.com>
2025-03-08 14:31:21 +0000

    is square skeleton

---
+++ squares/src/main.rs
@@ -0,0 +1,16 @@
+fn is_square(n: i64) -> bool {
+    todo!()
+}
+fn main() {
+    println!("=== Square of Squares ===");
+}
+
+#[cfg(test)]
+mod test {
+    use super::is_square;
+
+    #[test]
+    fn test_is_square() {
+        todo!();
+    }
+}
