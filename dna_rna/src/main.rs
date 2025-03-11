8648ddedbb8fbdc310f2cf6bf6c82438bf4716b1
Memnoc <smartdroidesign@gmail.com>
2025-03-11 05:37:30 +0000

    dna to rna skeleton

---
+++ dna_rna/src/main.rs
@@ -0,0 +1,17 @@
+fn dna_to_rna(dna: &str) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("=== DNA to RNA conversion ===");
+}
+
+#[cfg(test)]
+mod test {
+    use super::dna_to_rna;
+
+    #[test]
+    fn test_dna_to_rna() {
+        todo!();
+    }
+}
