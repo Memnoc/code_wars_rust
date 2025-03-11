3b22b479e7fb75e7a908432799596c128d0071ca
Memnoc <smartdroidesign@gmail.com>
2025-03-11 05:29:40 +0000

    some notes

--- squares/src/main.rs
+++ squares/src/main.rs
@@ -1,6 +1,9 @@
 fn is_square(n: i64) -> bool {
     todo!()
 }
+
+// TODO: given an integral number, determine if it is a square number
+// We are gonna pause this one until we find it again
 fn main() {
     println!("=== Square of Squares ===");
 }
