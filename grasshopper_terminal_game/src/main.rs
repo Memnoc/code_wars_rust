4b89a446a4f65840ee8ab604668a80fd5056910b
Memnoc <smartdroidesign@gmail.com>
2025-03-15 15:45:15 +0000

    grasshopper terminal game - skeleton

---
+++ grasshopper_terminal_game/src/main.rs
@@ -0,0 +1,20 @@
+// HEADER: my solution
+fn move_hero(position: i32, roll: u32) -> u32 {
+    unimplemented!()
+}
+
+// TODO: for an hypothetical game:
+// create a function that takes the current position of the hero
+// and the roll (1-6) and return the new position
+fn main() {
+    println!("=== Grasshopper Terminal game move function ===");
+}
+
+#[cfg(test)]
+mod test {
+    use super::move_hero;
+    #[test]
+    fn test_move_hero() {
+        todo!();
+    }
+}
