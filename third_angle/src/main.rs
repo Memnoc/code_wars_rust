378dc3194b781b994e3d2315893012c40cd88066
Memnoc <smartdroidesign@gmail.com>
2025-03-16 11:59:54 +0000

    third angle - init

---
+++ third_angle/src/main.rs
@@ -0,0 +1,17 @@
+// HEADER: my solution
+fn other_angle(a: u32, b: u32) -> u32 {
+    todo!()
+}
+fn main() {
+    println!("=== Third angle of a triangle ===");
+}
+
+#[cfg(test)]
+mod test {
+    use super::other_angle;
+
+    #[test]
+    fn test_other_angle() {
+        todo!();
+    }
+}
