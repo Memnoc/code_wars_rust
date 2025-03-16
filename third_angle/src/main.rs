37d5d155ce1584536773c50d32b3353ba607a615
Memnoc <smartdroidesign@gmail.com>
2025-03-16 12:12:32 +0000

    third angle - my solution

--- third_angle/src/main.rs
+++ third_angle/src/main.rs
@@ -1,9 +1,10 @@
 // HEADER: my solution
 fn other_angle(a: u32, b: u32) -> u32 {
-    todo!()
+    180 - a - b
 }
 fn main() {
     println!("=== Third angle of a triangle ===");
+    other_angle(30, 60);
 }
 
 #[cfg(test)]
@@ -12,6 +13,10 @@ mod test {
 
     #[test]
     fn test_other_angle() {
-        todo!();
+        assert_eq!(other_angle(30, 60), 90);
+        assert_eq!(other_angle(60, 60), 60);
+        assert_eq!(other_angle(43, 78), 59);
+        assert_eq!(other_angle(30, 60), 90);
+        assert_eq!(other_angle(10, 20), 150);
     }
 }
