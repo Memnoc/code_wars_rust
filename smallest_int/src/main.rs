8f9b7ed09826b600b4c5e91ed6aa09358ec3b585
Memnoc <smartdroidesign@gmail.com>
2025-03-17 18:10:05 +0000

    find smallest int - my solution

--- smallest_int/src/main.rs
+++ smallest_int/src/main.rs
@@ -1,8 +1,11 @@
 fn find_smallest_int(arr: &[i32]) -> i32 {
-    todo!()
+    *arr.iter().min().unwrap_or(&i32::MAX)
 }
+// TODO: pretty easy but with the tricky bit of handling memory
 fn main() {
     println!("=== Find smalles int ===");
+    let test_array = [34, 15, 88, 2]; // expecting 2
+    println!("{}", find_smallest_int(&test_array));
 }
 
 #[cfg(test)]
