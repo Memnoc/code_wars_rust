877954796a3e8be18d380b84e6c2e38bc5df6abe
Memnoc <smartdroidesign@gmail.com>
2025-03-18 06:37:50 +0000

    sort and star - init

---
+++ sort_and_star/src/main.rs
@@ -0,0 +1,34 @@
+// HEADER: my solution
+fn two_sort(arr: &[&str]) -> String {
+    todo!()
+}
+// NOTE: sort alphabetically a list of strings
+// case-sensitive and based on the ASCII value of the chars
+// return the first value
+// return must be a string
+// return must have *** between each letter
+fn main() {
+    println!("=== Sort and Star ===");
+}
+
+#[cfg(test)]
+mod test {
+    use super::two_sort;
+
+    #[test]
+    fn test_two_sort() {
+        assert_eq!(
+            two_sort(&[
+                "bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"
+            ]),
+            "b***i***t***c***o***i***n"
+        );
+        assert_eq!(
+            two_sort(&[
+                "turns", "out", "random", "test", "cases", "are", "easier", "than", "writing",
+                "out", "basic", "ones"
+            ]),
+            "a***r***e"
+        );
+    }
+}
